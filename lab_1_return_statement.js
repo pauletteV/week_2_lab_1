ff95b2e51a680f5cc2433e2b1960b6be24f227a3
Paulette <pvallad@picnicnewmedia.com>
2016-10-14 13:21:20 -0400

    tried a local variable with function

--- lab_1_return_statement.js
+++ lab_1_return_statement.js
@@ -1,5 +1,8 @@
 
-//personal challenge: make a local variable that holds the total & return it to total so the global variable isn't changed inside of a function
+//personal challenge: do the calculating of the grand total in a local variable & then pass that to the global variable;
+
+/* problems: in order += the prices to a variable, THAT variable somehow has to be initalized to 0... once
+*/
 var total;
 
 var groceryList = [
@@ -34,12 +37,27 @@ var groceryList = [
 //console.log(groceryList);
 
 groceryList.forEach(function(element) {
-	console.log(element.name + " " + "$" + element.price);
-	var innerTotal;
-	innerTotal += element.price;
-	console.log("The value of innerTotal is: " + innerTotal); //returning NaN
-	//return innerTotal;
+	//console.log(element.name + " " + "$" + element.price);
+	/*total += element.price;*/
+	calcTotal(element.price);
 });
 
 //console.log("total is: $" + total);
 
+
+// the function below does not work because itemPrice will ALWAYS be undefined... the variable gets created each time we are calling the function
+function calcTotal (price) {
+	var itemPrice;
+	console.log("item price is: " + itemPrice);
+
+	//if item price is undefined give it the value of the passed in value for price
+	if (!itemPrice) {   
+		itemPrice = price;
+		console.log("if itemPrice was not defined " + itemPrice);
+	} 
+	else {
+		itemPrice += price;
+		console.log("running total itemPrice: " + itemPrice);
+	}
+
+}
